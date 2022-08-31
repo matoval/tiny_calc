af3119e9abf009990129d4b233287119bf800698
matoval <mat24c@gmail.com>
2022-08-31 13:38:51 -0700

    only allow calc chars

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ const addAllParams = e => {
   changeScreen(screen.value, e.target.value)
 }
 
+const isAllowedChar = (char) => {
+  const allowedChar = ['+','-','*','/','','=','0','1','2','3','4','5','6','7','8','9']
+  return [...char].every( c => [...allowedChar].includes(c))
+}
+
 const handleEvent = (acc, cur) => {
+  if (acc && !isAllowedChar(acc)) {
+    return ""
+  }
   if (cur === ""){
     return""
   }
